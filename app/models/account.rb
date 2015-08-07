fdba22f9e405d52429d62291684dbc94716cddd7
account <account@tk2-209-14108.vs.sakura.ne.jp>
2015-08-08 00:54:31 +0900

    add validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,6 @@
 class Account < ActiveRecord::Base
+  validates :date, presence: true, format: { with: /\d{4}-\d{2}-\d{2}/ }
+  validates :content, presence: true
+  validates :category, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
