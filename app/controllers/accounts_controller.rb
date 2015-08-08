6cabc0669e0ffa90936ae20cae682f0ff26f40e1
account <account@tk2-209-14108.vs.sakura.ne.jp>
2015-08-08 11:16:29 +0900

    implement create

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,74 @@
 class AccountsController < ApplicationController
+  def create
+    result = check_request_body params
+    if result.empty?
+      account = Account.new
+      result, obj = account.create
+      if result
+        render :status => :created, :json => obj
+      else
+        errors = obj.map{|param| :error_code => "invalid_value_#{param.to_s}" }
+        render :status => :bad_request, :json => errors
+      end
+    else
+      errors = result.map{|param| :error_code => "absent_param_#{param.to_s}" }
+      render :status => :bad_request, :json => errors
+    end
+  end
+
+  def read
+    if check_request_body params
+
+    else
+
+    end
+  end
+
+  def update
+    if check_request_body params
+
+    else
+
+    end
+  end
+
+  def delete
+    if check_request_body params
+
+    else
+
+    end
+  end
+
+  def settle
+    if check_request_body params
+
+    else
+
+    end
+  end
+
+  private
+
+  def check_request_body(request_params)
+    absent_params = []
+    case request_params[:action]
+    when :create
+      absent_params << :date unless request_params[:date]
+      absent_params << :content unless request_params[:content]
+      absent_params << :category unless request_params[:category]
+      absent_params << :price unless request_params[:price]
+    when :read
+      return true
+    when :update
+      return true
+    when :delete
+      return true
+    when :settle
+      return true
+    else
+      return false
+    end
+    absent_params
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,14 @@
 class Account < ActiveRecord::Base
-  validates :date, presence: true, format: { with: /\d{4}-\d{2}-\d{2}/ }
-  validates :content, presence: true
-  validates :category, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :date, :presence => true, :format => { :with => /\d{4}-\d{2}-\d{2}/ }
+  validates :content, :presence => true
+  validates :category, :presence => true
+  validates :price, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+
+  def create
+    account = Account.new(:date => params[:date], :content => params[:content], :category => params[:category], :price => params[:price])
+    if account.save
+      return [true, account]
+    else
+      return [false, account.errors.messages.keys]
+  end
 end
