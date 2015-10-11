e2e519b5b0f5f3a8712a8f332e602f78f0cef49b
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2015-10-11 12:29:48 +0900

    implement exception test

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -48,7 +48,7 @@ class AccountTest < ActiveSupport::TestCase
   end
 
   test 'should return updated accounts(account_type)' do
-    result = Account.new.update({
+    result = Account.update({
       :condition => {:account_type => 'expense'},
       :with => {:account_type => 'income'},
     })
@@ -66,9 +66,9 @@ class AccountTest < ActiveSupport::TestCase
     }
     expected_account = Account.create!(attributes)
     expected_account.date = now - 24 * 60 * 60
-    result = Account.new.update({
+    result = Account.update({
       :condition => {:date => now.strftime('%Y-%m-%d')},
-      :with => {:date => now - 24 * 60 * 60},
+      :with => {:date => (now - 24 * 60 * 60).strftime('%Y-%m-%d')},
     })
     assert_equal [true, [expected_account]], [result.first, result.last.to_a]
     Account.delete_all
@@ -80,7 +80,7 @@ class AccountTest < ActiveSupport::TestCase
     expense = accounts(:expense)
     expense.content = '更新後データ'
     expected_accounts = [expense, income]
-    result = Account.new.update({
+    result = Account.update({
       :condition => {:content => 'テスト用データ'},
       :with => {:content => '更新後データ'},
     })
@@ -93,7 +93,7 @@ class AccountTest < ActiveSupport::TestCase
     expense = accounts(:expense)
     expense.category = '更新'
     expected_accounts = [expense, income]
-    result = Account.new.update({
+    result = Account.update({
       :condition => {:category => 'テスト'},
       :with => {:category => '更新'},
     })
@@ -106,7 +106,7 @@ class AccountTest < ActiveSupport::TestCase
     expense = accounts(:expense)
     expense.price = 1000
     expected_accounts = [expense, income]
-    result = Account.new.update({
+    result = Account.update({
       :condition => {:price => 100},
       :with => {:price => 1000},
     })
@@ -118,7 +118,7 @@ class AccountTest < ActiveSupport::TestCase
     expense.account_type = 'income'
     expense.category = 'テスト'
     expense.price = 10000
-    result = Account.new.update({
+    result = Account.update({
       :condition => {
         :account_type => 'expense',
         :price => 100,
@@ -133,7 +133,7 @@ class AccountTest < ActiveSupport::TestCase
   end
 
   test 'should return no updated accounts' do
-    result = Account.new.update({
+    result = Account.update({
       :condition => {
         :account_type => 'expense',
         :price => 1,
@@ -153,7 +153,7 @@ class AccountTest < ActiveSupport::TestCase
     expense = accounts(:expense)
     expense.account_type = 'income'
     expense.price = 100000
-    result = Account.new.update({
+    result = Account.update({
       :with => {
         :account_type => 'income',
         :price => 100000,
@@ -163,32 +163,32 @@ class AccountTest < ActiveSupport::TestCase
   end
 
   test 'should return empty array due to success(account_type)' do
-    result = Account.new.destroy({:account_type => 'income'})
+    result = Account.destroy({:account_type => 'income'})
     assert_equal [true, []], result
   end
 
   test 'should return empty array due to success(date)' do
-    result = Account.new.destroy({:date => '1000-01-01'})
+    result = Account.destroy({:date => '1000-01-01'})
     assert_equal [true, []], result
   end
 
   test 'should return empty array due to success(content)' do
-    result = Account.new.destroy({:content => 'テスト用データ'})
+    result = Account.destroy({:content => 'テスト用データ'})
     assert_equal [true, []], result
   end
 
   test 'should return empty array due to success(category)' do
-    result = Account.new.destroy({:category => 'テスト'})
+    result = Account.destroy({:category => 'テスト'})
     assert_equal [true, []], result
   end
 
   test 'should return empty array due to success(price)' do
-    result = Account.new.destroy({:price => 100})
+    result = Account.destroy({:price => 100})
     assert_equal [true, []], result
   end
 
   test 'should return empty array due to success(date, price)' do
-    result = Account.new.destroy({
+    result = Account.destroy({
       :date => '1000-01-01',
       :price => 100,
     })
@@ -196,7 +196,7 @@ class AccountTest < ActiveSupport::TestCase
   end
 
   test 'should return empty array due to success but no deleted accounts(date, price)' do
-    result = Account.new.destroy({
+    result = Account.destroy({
       :date => '1000-01-01',
       :price => 1,
     })
@@ -204,22 +204,164 @@ class AccountTest < ActiveSupport::TestCase
   end
 
   test 'should return empty array due to success' do
-    result = Account.new.destroy
+    result = Account.destroy
     assert_equal [true, []], result
   end
 
   test 'should return settlements(yearly)' do
-    result = Account.new.settle('yearly')
+    result = Account.settle('yearly')
     assert_equal [true, {'1000' => 0}], result
   end
 
   test 'should return settlements(monthly)' do
-    result = Account.new.settle('monthly')
+    result = Account.settle('monthly')
     assert_equal [true, {'1000-01' => 0}], result
   end
 
   test 'should return settlements(daily)' do
-    result = Account.new.settle('daily')
+    result = Account.settle('daily')
     assert_equal [true, {'1000-01-01' => 0}], result
+  end
+
+  test 'should return invalid conditions when show(account_type)' do
+    result = Account.show(:account_type => 'invalid_type')
+    assert_equal [false, [:account_type]], [result.first, result.last.to_a]    
+  end
+
+  test 'should return invalid conditions when show(date)' do
+    result = Account.show(:date => '1000-00-00')
+    assert_equal [false, [:date]], [result.first, result.last.to_a]    
+  end
+
+  test 'should return invalid conditions when show(price)' do
+    result = Account.show(:price => -100)
+    assert_equal [false, [:price]], [result.first, result.last.to_a]    
+  end
+
+  test 'should return invalid conditions when show(account_type, date, price)' do
+    result = Account.show({
+      :account_type => 'invalid_type',
+      :date => Time.now.strftime('%Y-%m-%d'),
+      :price => -100,
+    })
+    assert_equal [false, [:account_type, :price]], [result.first, result.last.to_a]    
+  end
+
+  test 'should return invalid conditions when update(condition:account_type)' do
+    result = Account.update({
+      :condition => {:account_type => 'invalid_type'},
+      :with => {:account_type => 'income'},
+    })
+    assert_equal [false, [:account_type]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(condition:date)' do
+    result = Account.update({
+      :condition => {:date => 'invalid_date'},
+      :with => {:category => 'テスト'},
+    })
+    assert_equal [false, [:date]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(condition:price)' do
+    result = Account.update({
+      :condition => {:price => 'invalid_price'},
+      :with => {:price => 100},
+    })
+    assert_equal [false, [:price]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(condition:account_type, price)' do
+    result = Account.update({
+      :condition => {
+        :account_type => 'income',
+        :price => -100,
+      },
+      :with => {:account_type => 'expense'},
+    })
+    assert_equal [false, [:price]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(with:account_type)' do
+    result = Account.update({
+      :condition => {:account_type => 'expense'},
+      :with => {:account_type => 'invalid_type'},
+    })
+    assert_equal [false, [:account_type]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(with:date)' do
+    result = Account.update({
+      :condition => {:category => 'テスト'},
+      :with => {:date => '1000-00-00'},
+    })
+    assert_equal [false, [:date]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(with:price)' do
+    result = Account.update({
+      :condition => {:price => 100},
+      :with => {:price => -100},
+    })
+    assert_equal [false, [:price]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(with:account_type, date)' do
+    result = Account.update({
+      :condition => {
+        :account_type => 'income',
+        :category => 'テスト',
+      },
+      :with => {
+        :account_type => 'invalid_type',
+        :date => 'invalid_date',
+      },
+    })
+    assert_equal [false, [:account_type, :date]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when update(with: )' do
+    result = Account.update({
+      :condition => {
+        :account_type => 'expense',
+        :category => 'テスト',
+      },
+      :with => {},
+    })
+    assert_equal [false, [:with]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when delete(account_type)' do
+    result = Account.destroy({:account_type => 'invalid_type'})
+    assert_equal [false, [:account_type]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when delete(date)' do
+    result = Account.destroy({:date => '1000-00-00'})
+    assert_equal [false, [:date]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when delete(price)' do
+    result = Account.destroy({:price => -100})
+    assert_equal [false, [:price]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when delete(account_type, date)' do
+    result = Account.destroy({
+      :account_type => 'invalid_type',
+      :date => 'invalid_date',
+      :price => 100
+    })
+    assert_equal [false, [:account_type, :date]], [result.first, result.last.to_a]
+  end
+
+  test 'should return invalid conditions when settle(invalid interval)' do
+    result = Account.settle('invalid_interval')
+    assert_nil result
+  end
+
+  test 'should return invalid conditions when settle(interval is absent)' do
+    result = Account.settle(nil)
+    assert_nil result
   end
 end
