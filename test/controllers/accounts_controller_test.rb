8d3752eb923cd705735b9c58d6c050032f756bca
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2015-10-17 10:13:58 +0900

    implement exception test

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -535,4 +535,278 @@ class AccountsControllerTest < ActionController::TestCase
     get :settle, {:interval => 'daily'}
     assert_equal({'1000-01-01' => 0}, JSON.parse(@response.body))
   end
+
+  [:account_type, :date, :content, :category, :price].each do |delete_column|
+    test "should send error code when create(#{delete_column})" do
+      created_account = {
+        :account_type => 'income',
+        :date => '1000-01-01',
+        :content => 'システムテスト用データ',
+        :category => 'システムテスト',
+        :price => 100,
+      }.delete_if{|k, v| k == delete_column }
+
+      post :create, {:accounts => created_account}
+
+      assert_response :bad_request
+
+      parsed_body = JSON.parse(@response.body)
+      assert_equal 1, parsed_body.size
+      assert_equal({'error_code' => "absent_param_#{delete_column}"}, parsed_body.first)
+    end
+  end
+
+  test 'should send error code when create(date, price)' do
+    created_account = {
+      :account_type => 'income',
+      :content => 'システムテスト用データ',
+      :category => 'システムテスト',
+    }
+
+    post :create, {:accounts => created_account}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 2, parsed_body.size
+    [:date, :price].each_with_index do |key, i|
+      assert_equal({'error_code' => "absent_param_#{key}"}, parsed_body[i])
+    end
+  end
+
+  test 'should send error code when create(account_type, date, content, category, price)' do
+    post :create, {:accounts => {}}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 5, parsed_body.size
+    [:account_type, :date, :content, :category, :price].each_with_index do |key, i|
+      assert_equal({'error_code' => "absent_param_#{key}"}, parsed_body[i])
+    end
+  end
+
+  test 'should send error code when create(accounts)' do
+    post :create
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'absent_param_accounts'}, parsed_body.first)
+  end
+
+  [
+   {:account_type => 'invalid_type'},
+   {:date => 'invalid_date'},
+   {:price => 'invalid_price'},
+  ].each do |invalid_param|
+    key = invalid_param.keys.first
+    test "should send error code when create with invalid #{key}" do
+      created_account = {
+        :account_type => 'expense',
+        :date => '1000-01-01',
+        :content => 'システムテスト用データ',
+        :category => 'システムテスト',
+        :price => 100,
+      }.merge(invalid_param)
+
+      post :create, {:accounts => created_account}
+
+      assert_response :bad_request
+
+      parsed_body = JSON.parse(@response.body)
+      assert_equal 1, parsed_body.size
+      assert_equal({'error_code' => "invalid_value_#{key}"}, parsed_body.first)
+    end
+  end
+
+  test 'should send error code when create with invalid account_type and price' do
+    created_account = {
+      :account_type => 'invalid_type',
+      :date => '1000-01-01',
+      :content => 'システムテスト用データ',
+      :category => 'システムテスト',
+      :price => 'invalid_price',
+    }
+
+    post :create, {:accounts => created_account}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 2, parsed_body.size
+    [:account_type, :price].each_with_index do |key, i|
+      assert_equal({'error_code' => "invalid_value_#{key}"}, parsed_body[i])
+    end
+  end
+
+  [
+   {:account_type => 'invalid_type'},
+   {:date => 'invalid_date'},
+   {:price => 'invalid_price'},
+  ].each do |invalid_param|
+    key = invalid_param.keys.first
+    test "should send error code when index with invalid #{key}" do
+      get :read, invalid_param
+
+      assert_response :bad_request
+
+      parsed_body = JSON.parse(@response.body)
+
+      assert_equal 1, parsed_body.size
+      assert_equal({'error_code' => "invalid_value_#{key}"}, parsed_body.first)
+    end
+  end
+
+  test 'should send error code when update with invalid condition(account_type)' do
+    put :update, {
+      :condition => {:account_type => 'invalid_type'},
+      :with => {:account_type => 'expense'},
+    }
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_account_type'}, parsed_body.first)
+  end
+
+  test 'should send error code when update with invalid condition(date)' do
+    put :update, {
+      :condition => {:date => '01-01-1000'},
+      :with => {:price => 1000},
+    }
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_date'}, parsed_body.first)
+  end
+
+  test 'should send error code when update with invalid condition(price)' do
+    put :update, {
+      :condition => {:price => -1},
+      :with => {:account_type => 'expense'},
+    }
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_price'}, parsed_body.first)
+  end
+
+  test 'should send error code when update with invalid value(account_type)' do
+    put :update, {:with => {:account_type => 'invalid_type'}}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_account_type'}, parsed_body.first)
+  end
+
+  test 'should send error code when update with invalid value(date)' do
+    put :update, {:with => {:date => 'invalid_date'}}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_date'}, parsed_body.first)
+  end
+
+  test 'should send error code when update with invalid value(price)' do
+    put :update, {:with => {:price => 100.5}}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_price'}, parsed_body.first)
+  end
+
+  test 'should send error code when update without value' do
+    put :update, {:with => {}}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_with'}, parsed_body.first)
+  end
+
+  test 'should send error code when update without with parameter' do
+    put :update
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'absent_param_with'}, parsed_body.first)
+  end
+
+  test 'should send error code when delete with invalid condition(accout_type)' do
+    delete :delete, {:account_type => 'invalid_type'}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_account_type'}, parsed_body.first)
+  end
+
+  test 'should send error code when delete with invalid condition(date)' do
+    delete :delete, {
+      :date => 'invalid_date',
+      :price => 100,
+    }
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_date'}, parsed_body.first)
+  end
+
+  test 'should send error code when delete with invalid condition(price)' do
+    delete :delete, {
+      :category => 'システムテスト',
+      :price => 'invalid_price',
+    }
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_price'}, parsed_body.first)
+  end
+
+  test 'should send error code when settle with invalid interval' do
+    get :settle, {:interval => 'invalid_interval'}
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'invalid_value_interval'}, parsed_body.first)
+  end
+
+  test 'should send error code when settle without interval' do
+    get :settle
+
+    assert_response :bad_request
+
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 1, parsed_body.size
+    assert_equal({'error_code' => 'absent_param_interval'}, parsed_body.first)
+  end
 end
