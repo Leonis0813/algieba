859c654f0d44453de6e030e2bf3f5f9aab20cd7b
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-21 13:58:27 +0900

    create view spec(alsp remove .keep)

---
+++ spec/views/accounts/register.html.erb_spec.rb
@@ -0,0 +1,49 @@
+# coding: utf-8
+require 'rails_helper'
+
+describe "accounts/register", :type => :view do
+  before(:all) do
+    @response = nil
+    @account = Account.new
+    @all_accounts = Account.all
+  end
+
+  before(:each) do
+    render
+    @response ||= response
+  end
+
+  it '<form>タグがあること' do
+    expect(@response).to have_selector('form[action="/accounts"][data-remote="true"][method="post"]')
+  end
+
+  %w[ date content category price ].each do |attribute|
+    it "accounts[#{attribute}]を含む<input>タグがあること" do
+      expect(@response).to have_selector("input[type='text'][name='accounts[#{attribute}]']", :text => '')
+    end
+  end
+
+  %w[ income expense ].each do |account_type|
+    it "value=#{account_type}を持つラジオボタンがあること" do
+      expect(@response).to have_selector("input[type='radio'][value='#{account_type}']")
+    end
+  end
+
+  it '支出が選択されていること' do
+    expect(@response).to have_selector('input[type="radio"][value="expense"][checked="checked"]')
+  end
+
+  it '<table>タグがあること' do
+    expect(@response).to have_selector('table[width="100%"]')
+  end
+
+  %w[ 種類 日付 内容 カテゴリ 金額 ].each do |header|
+    it "<table>タグ内に<td>#{header}</td>があること" do
+      expect(@response).to have_xpath("//table/tr/td[text()='#{header}']")
+    end
+  end
+
+  it '<tr id="append">があること' do
+    expect(@response).to have_xpath('//table/tr[@id="append"]')
+  end
+end
