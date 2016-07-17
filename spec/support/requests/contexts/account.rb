ca00ab091d14f8b4b030bf36de82e964cf4d6c7a
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-07-17 23:44:39 +0900

    modify port for test

--- spec/support/requests/contexts/account.rb
+++ spec/support/requests/contexts/account.rb
@@ -2,7 +2,7 @@
 
 shared_context '共通設定' do
   before(:all) do
-    @base_url = 'http://160.16.66.112:3000'
+    @base_url = 'http://160.16.66.112:88'
     @content_type_json = {'Content-Type' => 'application/json'}
     @hc = HTTPClient.new
     @attributes = %w[ account_type date content category price ]
