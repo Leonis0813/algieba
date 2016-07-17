a425754ec87b1a6014c5795ea90619a46f861d7d
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-07-17 15:51:42 +0900

    add test case

--- spec/requests/index_accounts_spec.rb
+++ spec/requests/index_accounts_spec.rb
@@ -23,26 +23,31 @@ describe '家計簿を検索する', :type => :request do
         before(:all) { @created_accounts << @pbody }
         it_behaves_like 'Request: 家計簿が正しく登録されていることを確認する'
 
-        describe '家計簿を更新する' do
-          before(:all) { @id = @created_accounts.last['id'] }
-          include_context 'PUT /accounts/[:id]', :account_type => 'income'
-          it_behaves_like 'Request: 家計簿が正しく更新されていることを確認する'
+        describe '家計簿を検索する' do
+          include_context 'GET /accounts'
+          it_behaves_like 'Request: 家計簿が正しく検索されていることを確認する'
 
-          describe '家計簿を検索する' do
-            include_context 'GET /accounts', :account_type => 'income'
-            it_behaves_like 'Request: 家計簿が正しく検索されていることを確認する'
-          end
-
-          describe '家計簿を削除する' do
-            valid_accounts.size.times do |i|
-              before(:all) { @id = @created_accounts[i]['id'] }
-              include_context 'DELETE /accounts/[:id]'
-              it_behaves_like 'Request: 家計簿が正しく削除されていることを確認する'
-            end
+          describe '家計簿を更新する' do
+            before(:all) { @id = @created_accounts.last['id'] }
+            include_context 'PUT /accounts/[:id]', :account_type => 'income'
+            it_behaves_like 'Request: 家計簿が正しく更新されていることを確認する'
 
             describe '家計簿を検索する' do
-              include_context 'GET /accounts'
+              include_context 'GET /accounts', :account_type => 'income'
               it_behaves_like 'Request: 家計簿が正しく検索されていることを確認する'
+
+              describe '家計簿を削除する' do
+                valid_accounts.size.times do |i|
+                  before(:all) { @id = @created_accounts[i]['id'] }
+                  include_context 'DELETE /accounts/[:id]'
+                  it_behaves_like 'Request: 家計簿が正しく削除されていることを確認する'
+                end
+
+                describe '家計簿を検索する' do
+                  include_context 'GET /accounts'
+                  it_behaves_like 'Request: 家計簿が正しく検索されていることを確認する'
+                end
+              end
             end
           end
         end
