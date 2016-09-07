921b0e823013055f80883c97e0cc8a2967e65cbc
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-09-07 22:13:29 +0900

    create model for index validation

---
+++ app/models/index.rb
@@ -0,0 +1,32 @@
+class Index
+  include ActiveModel::Model
+
+  attr_accessor :account_type
+  attr_accessor :date_before, :date_after
+  attr_accessor :content_equal, :content_include
+  attr_accessor :category
+  attr_accessor :price_upper, :price_lower
+
+  validates :account_type, :inclusion => {:in => %w[ income expense ]}, :allow_nil => true
+  validates :price_upper, :price_lower, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}, :allow_nil => true
+  validate :date_valid?
+
+  def date_valid?
+    return unless date_before or date_after
+
+    [[:date_before, date_before], [:date_after, date_after]].each do |date_symbol, date_value|
+      begin
+        errors.add(date_symbol, 'invalid') if date_value and not date_value.match(/\A\d{4}-\d{2}-\d{2}\z/)
+        Date.parse(date_value) if date_value
+      rescue ArgumentError => e
+        errors.add(date_symbol, 'invalid')
+      end
+    end
+    return if errors.any?
+
+    if date_before and date_after and Date.parse(date_before) > Date.parse(date_after)
+      errors.add(:date_before, 'invalid')
+      errors.add(:date_after, 'invalid')
+    end
+  end
+end
