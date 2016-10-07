13a5d658ce079c052580174c002516c3a359ea88
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-10-08 03:50:38 +0900

    puts if duplicate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 # coding: utf-8
-Account.create(:id => 1, :account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100)
-Account.create(:id => 2, :account_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100)
+[
+  Account.new(:id => 1, :account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100),
+  Account.new(:id => 2, :account_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100),
+].each do |account|
+  begin
+    account.save!
+  rescue ActiveRecord::RecordNotUnique => e
+    puts "[Warning] #{e.message}"
+  end
+end
