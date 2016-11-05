610b43ce7c5bab195cedcf7995e553c043f565f7
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-11-05 16:03:44 +0900

    add routing for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :accounts, :except => [:new, :edit], :defaults => {:format => 'json'}
   get '/settlement' => 'accounts#settle', :defaults => {:format => 'json'}
   get '/' => 'accounts#manage'
+  get '/login' => 'login#form'
+  post '/login' => 'login#authenticate_user'
 end
