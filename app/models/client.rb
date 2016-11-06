34141b792885d7d4c95d38fb4e381d10ca53e140
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-11-06 12:53:01 +0900

    generate application info

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,17 @@
 class Client < ActiveRecord::Base
+  class << self
+    def generate_application_id
+      loop do
+        id = SecureRandom.hex(8)
+        return id unless Client.find_by(:application_id => id)
+      end
+    end
+
+    def generate_application_key
+      loop do
+        key = SecureRandom.hex(16)
+        return key unless Client.find_by(:application_key => key)
+      end
+    end
+  end
 end
