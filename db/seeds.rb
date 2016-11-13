f1e5e9d2def6def7069b2ae30223836d3b2d64d0
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-11-13 20:06:30 +0900

    add my user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,44 @@
 # coding: utf-8
-[
-  {:id => 1, :account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100},
-  {:id => 2, :account_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100},
-].each do |params|
-  begin
-    Account.create!(params)
-  rescue ActiveRecord::RecordNotUnique => e
-    puts "[Warning] #{e.message}"
+case Rails.env
+when 'development'
+  [
+    {:id => 1, :account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100},
+    {:id => 2, :account_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100},
+  ].each do |params|
+    begin
+      Account.create!(params)
+    rescue ActiveRecord::RecordNotUnique => e
+      puts "[Warning] #{e.message}"
+    end
   end
-end
 
-[
-  {:id => 1, :user_id => 'test_user_id', :password => 'test_user_pass'},
-].each do |params|
-  begin
-    User.create!(params)
-  rescue ActiveRecord::RecordNotUnique => e
-    puts "[Warning] #{e.message}"
+  [
+    {:id => 1, :user_id => 'test_user_id', :password => 'test_user_pass'},
+  ].each do |params|
+    begin
+      User.create!(params)
+    rescue ActiveRecord::RecordNotUnique => e
+      puts "[Warning] #{e.message}"
+    end
   end
-end
 
-[
-  {:id => 1, :application_id => 'test_app_id', :application_key => 'test_app_key'},
-].each do |params|
-  begin
-    Client.create!(params)
-  rescue ActiveRecord::RecordNotUnique => e
-    puts "[Warning] #{e.message}"
+  [
+    {:id => 1, :application_id => 'test_app_id', :application_key => 'test_app_key'},
+  ].each do |params|
+    begin
+      Client.create!(params)
+    rescue ActiveRecord::RecordNotUnique => e
+      puts "[Warning] #{e.message}"
+    end
+  end
+when 'production'
+  [
+    {:id => 1, :user_id => 'leonis', :password => '0qpalzm1'},
+  ].each do |params|
+    begin
+      User.create!(params)
+    rescue ActiveRecord::RecordNotUnique => e
+      puts "[Warning] #{e.message}"
+    end
   end
 end
