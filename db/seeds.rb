a59465a9e3f294c552113eddf2ab4cebd77958cd
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-11-13 14:26:43 +0900

    add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,30 @@
 # coding: utf-8
 [
-  Account.new(:id => 1, :account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100),
-  Account.new(:id => 2, :account_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100),
-].each do |account|
+  {:id => 1, :account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100},
+  {:id => 2, :account_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'zosma', :price => 100},
+].each do |params|
   begin
-    account.save!
+    Account.create!(params)
+  rescue ActiveRecord::RecordNotUnique => e
+    puts "[Warning] #{e.message}"
+  end
+end
+
+[
+  {:user_id => 'test_user_id', :password => 'test_user_pass'},
+].each do |params|
+  begin
+    User.create!(params)
+  rescue ActiveRecord::RecordNotUnique => e
+    puts "[Warning] #{e.message}"
+  end
+end
+
+[
+  {:application_id => 'test_app_id', :application_key => 'test_app_key'},
+].each do |params|
+  begin
+    Client.create!(params)
   rescue ActiveRecord::RecordNotUnique => e
     puts "[Warning] #{e.message}"
   end
