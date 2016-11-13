eb22e0fbd683fc84b46e23d0db23eba3b61a7913
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-11-13 15:44:05 +0900

    use selenium-webdriver

--- spec/requests/regist_from_view_spec.rb
+++ spec/requests/regist_from_view_spec.rb
@@ -6,10 +6,14 @@ describe 'ブラウザから操作する', :type => :request, :js => true do
   color = {'収入' => 'success', '支出' => 'danger'}
 
   shared_context '家計簿を登録する' do |inputs, account_type|
-    before(:each) do
-      inputs.each {|key, value| fill_in "accounts_#{key}", :with => value.to_s }
-      choose "accounts_account_type_#{account_type}"
-      find(:xpath, '//form/span/input[@value="登録"]').click
+    before(:all) do
+      inputs.each do |key, value|
+        element = @driver.find_element(:id, "accounts_#{key}")
+        element.clear
+        element.send_keys(value.to_s)
+      end
+      @driver.find_element(:id, "accounts_account_type_#{account_type}").click
+      @driver.find_element(:xpath, '//form/span/input[@value="登録"]').click
       sleep 1
     end
   end
@@ -29,55 +33,86 @@ describe 'ブラウザから操作する', :type => :request, :js => true do
     accounts.each {|account| http_client.delete("#{base_url}/accounts/#{account['id']}") }
   end
 
-  describe 'Webページを表示する' do
-    before(:each) { page.driver.browser.authenticate('dev', '.dev') }
-    before(:each) { visit '/' }
+  describe '管理画面を開く' do
+    before(:all) do
+      @driver = Selenium::WebDriver.for :firefox
+      @driver.get('http://160.16.66.112:3000')
+    end
 
-    %w[ date content category price ].each do |column|
-      it "入力欄(id=accounts_#{column})が全て空白であること" do
-        expect(page).to have_xpath("//form/span/input[@id='accounts_#{column}']", :text => '')
+    it 'ログイン画面にリダイレクトされていること' do
+      expect(URI.parse(@driver.current_url).path).to eq '/login'
+    end
+
+    describe 'ログインする' do
+      before(:all) do
+        @driver.find_element(:id, 'user_id').send_keys('test_user_id')
+        @driver.find_element(:id, 'password').send_keys('test_user_pass')
+        @driver.find_element(:id => 'login').click
       end
-    end
 
-    it '入力欄の下に家計簿が表示されていること' do
-      expect(page).to have_xpath('//table/tbody/tr', :count => Kaminari.config.default_per_page - 1)
-    end
+      it '管理画面が開いていること' do
+        expect(URI.parse(@driver.current_url).path).to eq '/'
+      end
 
-    it 'ページングボタンが表示されていないこと' do
-      expect(page).not_to have_xpath('//nav[@class="pagination"]')
-    end
+      %w[ date content category price ].each do |column|
+        it "入力欄(id=accounts_#{column})が全て空白であること" do
+          expect(@driver.find_element(:id, "accounts_#{column}").text).to eq ''
+        end
+      end
 
-    describe '家計簿を登録する' do
-      include_context '家計簿を登録する', default_inputs.merge(:date => 'invalid_date'), 'income'
+      it '入力欄の下に家計簿が表示されていること' do
+        expect(@driver.find_elements(:xpath, '//table/tbody/tr').size).to eq (Kaminari.config.default_per_page - 1)
+      end
 
-      it '家計簿の数が変わっていないこと' do
-        expect(page).to have_xpath('//table/tbody/tr', :count => Kaminari.config.default_per_page - 1)
-        expect(page).not_to have_xpath('//nav[@class="pagination"]')
+      it 'ページングボタンが表示されていないこと' do
+        expect{ @driver.find_element(:xpath, '//nav[@class="pagination"]') }.to raise_error Selenium::WebDriver::Error::NoSuchElementError
       end
 
       describe '家計簿を登録する' do
-        include_context '家計簿を登録する', default_inputs, 'expense'
+        include_context '家計簿を登録する', default_inputs.merge(:date => 'invalid_date'), 'income'
 
-        it '家計簿の数が1つ増えていること' do
-          expect(page).to have_xpath('//table/tbody/tr', :count => Kaminari.config.default_per_page)
-          expect(page).not_to have_xpath('//nav[@class="pagination"]')
+        it '家計簿の数が変わっていないこと' do
+          expect(@driver.find_elements(:xpath, '//table/tbody/tr').size).to eq (Kaminari.config.default_per_page - 1)
+        end
 
-          page.find_all(:xpath, '//table/tbody/tr').each do |account|
-            type = account.find_all('td').first.text
-            expect(account).to have_xpath("../tr[@class='#{color[type]}']")
-          end
+        it 'ページングボタンが表示されていないこと' do
+          expect{ @driver.find_element(:xpath, '//nav[@class="pagination"]') }.to raise_error Selenium::WebDriver::Error::NoSuchElementError
         end
 
         describe '家計簿を登録する' do
-          include_context '家計簿を登録する', default_inputs, 'income'
+          include_context '家計簿を登録する', default_inputs, 'expense'
 
-          it '表示されている家計簿の数が変わっていないこと' do
-            expect(page).to have_xpath('//table/tbody/tr', :count => Kaminari.config.default_per_page)
-            expect(page).to have_xpath('//nav[@class="pagination"]')
+          it '家計簿の数が1つ増えていること' do
+            expect(@driver.find_elements(:xpath, '//table/tbody/tr').size).to eq Kaminari.config.default_per_page
+          end
 
-            page.find_all(:xpath, '//table/tbody/tr').each do |account|
-              type = account.find_all('td').first.text
-              expect(account).to have_xpath("../tr[@class='#{color[type]}']")
+          it 'ページングボタンが表示されていること' do
+            expect(@driver.find_element(:xpath, '//nav[@class="pagination"]')).to be
+          end
+
+          it '背景色が正しいこと' do
+            @driver.find_elements(:xpath, '//table/tbody/tr').each do |element|
+              type = element.find_element(:xpath, './td').text
+              expect(element.find_element(:xpath, "../tr[@class='#{color[type]}']")).to be
+            end
+          end
+
+          describe '家計簿を登録する' do
+            include_context '家計簿を登録する', default_inputs, 'income'
+
+            it '表示されている家計簿の数が変わっていないこと' do
+              expect(@driver.find_elements(:xpath, '//table/tbody/tr').size).to eq Kaminari.config.default_per_page
+            end
+
+            it 'ページングボタンが表示されていること' do
+              expect(@driver.find_element(:xpath, '//nav[@class="pagination"]')).to be
+            end
+
+            it '背景色が正しいこと' do
+              @driver.find_elements(:xpath, '//table/tbody/tr').each do |element|
+                type = element.find_element(:xpath, './td').text
+                expect(element.find_element(:xpath, "../tr[@class='#{color[type]}']")).to be
+              end
             end
           end
         end
