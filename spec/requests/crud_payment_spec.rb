1e9430754f5461962c3029cafabe113ee321cfac
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-12-11 14:19:24 +0900

    rename account to payment for request spec

--- spec/requests/crud_payment_spec.rb
+++ spec/requests/crud_payment_spec.rb
@@ -2,64 +2,64 @@
 require 'rails_helper'
 
 describe '家計簿を管理する', :type => :request do
-  valid_account = {:account_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'algieba', :price => 100}
-  invalid_account = {:account_type => 'expense', :date => 'invalid_date', :category => 'algieba', :price => 100}
+  valid_payment = {:payment_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :category => 'algieba', :price => 100}
+  invalid_payment = {:payment_type => 'expense', :date => 'invalid_date', :category => 'algieba', :price => 100}
 
-  shared_context 'GET /accounts/[:id]' do
+  shared_context 'GET /payments/[:id]' do
     before(:all) do
       header = {'Authorization' => app_auth_header}
-      @res = http_client.get("#{base_url}/accounts/#{@id}", nil, header)
+      @res = http_client.get("#{base_url}/payments/#{@id}", nil, header)
       @pbody = JSON.parse(@res.body) rescue nil
     end
   end
 
   describe '家計簿を登録する' do
-    include_context 'POST /accounts', invalid_account
+    include_context 'POST /payments', invalid_payment
     it_behaves_like '400エラーをチェックする', ['absent_param_content']
 
     describe '家計簿を登録する' do
-      include_context 'POST /accounts', valid_account
-      before(:all) { @created_account = @pbody }
+      include_context 'POST /payments', valid_payment
+      before(:all) { @created_payment = @pbody }
       it_behaves_like 'ステータスコードが正しいこと', '201'
-      it_behaves_like 'レスポンスボディのキーが正しいこと', AccountHelper.response_keys
+      it_behaves_like 'レスポンスボディのキーが正しいこと', PaymentHelper.response_keys
 
       describe '家計簿を取得する' do
-        before(:all) { @id = @created_account['id'] }
-        include_context 'GET /accounts/[:id]'
+        before(:all) { @id = @created_payment['id'] }
+        include_context 'GET /payments/[:id]'
         it_behaves_like 'ステータスコードが正しいこと', '200'
-        it_behaves_like 'レスポンスボディのキーが正しいこと', AccountHelper.response_keys
+        it_behaves_like 'レスポンスボディのキーが正しいこと', PaymentHelper.response_keys
 
         describe '家計簿を更新する' do
           before(:all) do
             header = {'Authorization' => app_auth_header}.merge(content_type_json)
-            @res = http_client.put("#{base_url}/accounts/#{@created_account['id']}", {:account_type => 'income'}.to_json, header)
+            @res = http_client.put("#{base_url}/payments/#{@created_payment['id']}", {:payment_type => 'income'}.to_json, header)
             @pbody = JSON.parse(@res.body) rescue nil
           end
 
           it_behaves_like 'ステータスコードが正しいこと', '200'
-          it_behaves_like 'レスポンスボディのキーが正しいこと', AccountHelper.response_keys
+          it_behaves_like 'レスポンスボディのキーが正しいこと', PaymentHelper.response_keys
 
           it '家計簿が更新されていること' do
-            expect(@pbody['account_type']).to eq 'income'
+            expect(@pbody['payment_type']).to eq 'income'
           end
 
           describe '家計簿を検索する' do
-            include_context 'GET /accounts', {:account_type => 'income'}
+            include_context 'GET /payments', {:payment_type => 'income'}
             it_behaves_like 'ステータスコードが正しいこと', '200'
-            it_behaves_like 'レスポンスボディのキーが正しいこと', AccountHelper.response_keys
+            it_behaves_like 'レスポンスボディのキーが正しいこと', PaymentHelper.response_keys
 
             describe '家計簿を削除する' do
               before(:all) do
                 header = {'Authorization' => app_auth_header}
-                @res = http_client.delete("#{base_url}/accounts/#{@created_account['id']}", nil, header)
+                @res = http_client.delete("#{base_url}/payments/#{@created_payment['id']}", nil, header)
                 @pbody = JSON.parse(@res.body) rescue nil
               end
 
               it_behaves_like 'ステータスコードが正しいこと', '204'
 
               describe '家計簿を取得する' do
-                before(:all) { @id = @created_account['id'] }
-                include_context 'GET /accounts/[:id]'
+                before(:all) { @id = @created_payment['id'] }
+                include_context 'GET /payments/[:id]'
                 it_behaves_like 'ステータスコードが正しいこと', '404'
               end
             end
--- spec/requests/regist_from_view_spec.rb
+++ spec/requests/regist_from_view_spec.rb
@@ -5,14 +5,14 @@ describe 'ブラウザから操作する', :type => :request do
   default_inputs = {:date => '1000-01-01', :content => 'regist from view', :category => 'テスト', :price => 100}
   color = {'収入' => 'success', '支出' => 'danger'}
 
-  shared_context '家計簿を登録する' do |inputs, account_type|
+  shared_context '家計簿を登録する' do |inputs, payment_type|
     before(:all) do
       inputs.each do |key, value|
-        element = @driver.find_element(:id, "accounts_#{key}")
+        element = @driver.find_element(:id, "payments_#{key}")
         element.clear
         element.send_keys(value.to_s)
       end
-      @driver.find_element(:id, "accounts_account_type_#{account_type}").click
+      @driver.find_element(:id, "payments_payment_type_#{payment_type}").click
       @driver.find_element(:xpath, '//form/span/input[@value="登録"]').click
       sleep 1
     end
@@ -43,21 +43,21 @@ describe 'ブラウザから操作する', :type => :request do
 
   before(:all) do
     header = {'Authorization' => app_auth_header}
-    res = http_client.get("#{base_url}/accounts", nil, header)
+    res = http_client.get("#{base_url}/payments", nil, header)
     size = JSON.parse(res.body).size
-    account = default_inputs.merge(:account_type => 'income')
+    payment = default_inputs.merge(:payment_type => 'income')
 
     header = {'Authorization' => app_auth_header}.merge(content_type_json)
     (Kaminari.config.default_per_page - 1 - size).times do
-      http_client.post("#{base_url}/accounts", {:accounts => account}.to_json, header)
+      http_client.post("#{base_url}/payments", {:payments => payment}.to_json, header)
     end
   end
 
   after(:all) do
     header = {'Authorization' => app_auth_header}
-    res = http_client.get("#{base_url}/accounts", {:content_equal => 'regist from view'}, header)
-    accounts = JSON.parse(res.body)
-    accounts.each {|account| http_client.delete("#{base_url}/accounts/#{account['id']}", nil, header) }
+    res = http_client.get("#{base_url}/payments", {:content_equal => 'regist from view'}, header)
+    payments = JSON.parse(res.body)
+    payments.each {|payment| http_client.delete("#{base_url}/payments/#{payment['id']}", nil, header) }
   end
 
   describe '管理画面を開く' do
@@ -82,8 +82,8 @@ describe 'ブラウザから操作する', :type => :request do
       end
 
       %w[ date content category price ].each do |column|
-        it "入力欄(id=accounts_#{column})が全て空白であること" do
-          expect(@driver.find_element(:id, "accounts_#{column}").text).to eq ''
+        it "入力欄(id=payments_#{column})が全て空白であること" do
+          expect(@driver.find_element(:id, "payments_#{column}").text).to eq ''
         end
       end
 
