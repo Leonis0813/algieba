daf8ae3993a2cdb3928b5a0d607b4dce659ea2e5
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-12-13 23:54:47 +0900

    refactor spec

--- spec/views/payments/manage.html.erb_spec.rb
+++ spec/views/payments/manage.html.erb_spec.rb
@@ -36,32 +36,34 @@ describe "payments/manage", :type => :view do
   end
 
   shared_examples 'ページネーションが正しく表示されていること' do
+    nav_xpath = '//div[class="row row-center"]/div[id="pagination"]/nav[class="pagination"]'
+
     it 'ページングボタンが表示されていること' do
-      expect(html).to have_xpath("//div[@class='row row-center']/div[@id='pagination']/nav[@class='pagination']")
+      expect(html).to have_selector(nav_xpath)
     end
 
     it '先頭のページへのボタンが表示されていないこと' do
-      expect(html).not_to have_xpath("//nav/li/span[@class='first']/a", :text => I18n.t('views.pagination.first'))
+      expect(html).not_to have_selector("#{nav_xpath}/li/span[class='first']/a", :text => I18n.t('views.pagination.first'))
     end
 
     it '前のページへのボタンが表示されていないこと' do
-      expect(html).not_to have_xpath("//nav/li/span[@class='prev']/a", :text => I18n.t('views.pagination.previous'))
+      expect(html).not_to have_selector("#{nav_xpath}/li/span[class='prev']/a", :text => I18n.t('views.pagination.previous'))
     end
 
     it '1ページ目が表示されていること' do
-      expect(html).to have_xpath("//nav/li/span[@class='page current']", :text => 1)
+      expect(html).to have_selector("#{nav_xpath}/li/span[class='page current']", :text => 1)
     end
 
     it '2ページ目が表示されていること' do
-      expect(html).to have_xpath("//nav/li/span[@class='page']/a[@href='/?page=2']", :text => 2)
+      expect(html).to have_selector("#{nav_xpath}/li/span[class='page']/a[href='/?page=2']", :text => 2)
     end
 
     it '次のページへのボタンが表示されていること' do
-      expect(html).to have_xpath("//nav/li/span[@class='next']/a[@href='/?page=2']", :text => I18n.t('views.pagination.next'))
+      expect(html).to have_selector("#{nav_xpath}/li/span[class='next']/a[href='/?page=2']", :text => I18n.t('views.pagination.next'))
     end
 
     it '最後のページへのボタンが表示されていること' do
-      expect(html).to have_xpath("//nav/li/span[@class='last']/a", :text => I18n.t('views.pagination.last'))
+      expect(html).to have_selector("#{nav_xpath}/li/span[class='last']/a", :text => I18n.t('views.pagination.last'))
     end
   end
 
@@ -159,37 +161,39 @@ describe "payments/manage", :type => :view do
     end
   end
 
-  context '家計簿が1件登録されている場合' do
-    include_context 'HTML初期化'
-    include_context '家計簿を登録する', 1
+  describe '動的コンテンツのテスト' do
+    context "家計簿が#{per_page}件登録されている場合" do
+      include_context 'HTML初期化'
+      include_context '家計簿を登録する', per_page
 
-    it_behaves_like '表示されている家計簿の数が正しいこと', 1
-    it_behaves_like '家計簿の背景色が正しいこと'
+      it_behaves_like '表示されている家計簿の数が正しいこと', per_page
+      it_behaves_like '家計簿の背景色が正しいこと'
 
-    it 'ページングボタンが表示されていないこと' do
-      expect(html).not_to have_xpath("//div[@class='row row-center']/div[@id='pagination']/nav[@class='pagination']")
+      it 'ページングボタンが表示されていないこと' do
+        expect(html).not_to have_selector("//nav[class='pagination']")
+      end
     end
-  end
 
-  context "家計簿が#{per_page + 1}件登録されている場合" do
-    include_context 'HTML初期化'
-    include_context '家計簿を登録する', per_page + 1
+    context "家計簿が#{per_page + 1}件登録されている場合" do
+      include_context 'HTML初期化'
+      include_context '家計簿を登録する', per_page + 1
 
-    it_behaves_like '表示されている家計簿の数が正しいこと', per_page
-    it_behaves_like '家計簿の背景色が正しいこと'
-    it_behaves_like 'ページネーションが正しく表示されていること'
-  end
+      it_behaves_like '表示されている家計簿の数が正しいこと', per_page
+      it_behaves_like '家計簿の背景色が正しいこと'
+      it_behaves_like 'ページネーションが正しく表示されていること'
+    end
 
-  context "家計簿が#{per_page + 9}件登録されている場合" do
-    include_context 'HTML初期化'
-    include_context '家計簿を登録する', per_page + 9
+    context "家計簿が#{per_page + 9}件登録されている場合" do
+      include_context 'HTML初期化'
+      include_context '家計簿を登録する', per_page + 9
 
-    it_behaves_like '表示されている家計簿の数が正しいこと', per_page
-    it_behaves_like '家計簿の背景色が正しいこと'
-    it_behaves_like 'ページネーションが正しく表示されていること'
+      it_behaves_like '表示されている家計簿の数が正しいこと', per_page
+      it_behaves_like '家計簿の背景色が正しいこと'
+      it_behaves_like 'ページネーションが正しく表示されていること'
 
-    it 'リンクが省略されていること' do
-      expect(html).to have_xpath("//nav/li/span[@class='page gap']", :text => I18n.t('views.pagination.truncate'))
+      it 'リンクが省略されていること' do
+        expect(html).to have_selector("//nav/li/span[class='page gap']", :text => I18n.t('views.pagination.truncate'))
+      end
     end
   end
 end
