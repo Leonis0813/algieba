1a92d526eb4a2dda7bdf8fcefc050a701b2fbc79
Leonis0813 <Leonis.0813@gmail.com>
2017-04-09 10:35:38 +0100

    add test cases

---
+++ spec/controllers/category/index_spec.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+require 'rails_helper'
+
+describe CategoriesController, :type => :controller do
+  describe '正常系' do
+    context 'keywordを指定した場合' do
+
+    end
+
+    context 'keywordを指定しなかった場合' do
+
+    end
+  end
+
+  describe '異常系' do
+    context 'Authorizationヘッダーがない場合' do
+
+    end
+  end
+end
