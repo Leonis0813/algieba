f7ad17794a86840824dead4a16d0ff8d7b36fedc
Leonis0813 <Leonis.0813@gmail.com>
2017-04-09 02:15:38 +0100

    add route for indexing categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories, :only => [:index]
   resources :payments, :except => [:new, :edit], :defaults => {:format => 'json'}
   get '/settlement' => 'payments#settle', :defaults => {:format => 'json'}
   get '/' => 'payments#manage'
