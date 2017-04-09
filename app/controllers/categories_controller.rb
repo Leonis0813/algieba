43bf609bff1991bccd6b7172da7d7129eff6cac3
Leonis0813 <Leonis.0813@gmail.com>
2017-04-09 02:29:58 +0100

    create json builder

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def index
     query = params.permit(:keyword)
-    categories = Category.where(:name => query[:keyword])
+    @categories = Category.where(:name => query[:keyword])
 
     respond_to do |format|
       format.json {render :status => :ok, :template => 'categories/categories'}
