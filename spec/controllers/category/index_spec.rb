9ddda900fceeaa401094b7550bd178bc2eabbce0
Leonis0813 <Leonis.0813@gmail.com>
2017-04-23 00:47:25 +0900

    update shared context for test payment

--- spec/controllers/category/index_spec.rb
+++ spec/controllers/category/index_spec.rb
@@ -2,13 +2,27 @@
 require 'rails_helper'
 
 describe CategoriesController, :type => :controller do
-  describe '正常系' do
-    context 'keywordを指定した場合' do
+  shared_context 'カテゴリを検索する' do |keyword = ''|
+    before(:all) do
+      client.header('Authorization', app_auth_header)
+      @res = client.get('/categories.json', 'keyword' => keyword)
+      @pbody = JSON.parse(@res.body) rescue nil
+    end
+  end
 
+  include_context '事前準備: クライアントアプリを作成する'
+  include_context '事前準備: 家計簿を登録する'
+
+  describe '正常系' do
+    context 'keywordを指定した場合', :wip do
+      include_context 'カテゴリを検索する', 'algieba'
+      it '' do
+        p @pbody
+      end
     end
 
     context 'keywordを指定しなかった場合' do
-
+      include_context 'カテゴリを検索する'
     end
   end
 
--- spec/support/modules/shared/payment.rb
+++ spec/support/modules/shared/payment.rb
@@ -12,6 +12,17 @@ shared_context '事前準備: クライアントアプリを作成する' do
 end
 
 shared_context '事前準備: 家計簿を登録する' do
-  before(:all) { test_payment.each {|_, value| Payment.create!(value) } }
+  before(:all) do
+    test_payment.each do |_, value|
+      category_names = value[:category].split(',')
+      categories = category_names.map do |name|
+        Category.find_by_name(name) || Category.create!(:name => name)
+      end
+      payment = Payment.new(value.except(:category))
+      payment.categories = categories
+      payment.save!
+    end
+  end
+
   after(:all) { test_payment.each {|_, value| Payment.find_by(value[:id]).try(:delete) } }
 end
