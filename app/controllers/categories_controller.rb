9010acaa3f114fb32102b3ba4b23e9e66eed20d5
Leonis0813 <Leonis.0813@gmail.com>
2017-04-23 04:39:14 +0100

    require app auth

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :check_client
+
   def index
     query = params.permit(:keyword)
     @categories = query.empty? ? Category.all : Category.where(:name => query[:keyword])
