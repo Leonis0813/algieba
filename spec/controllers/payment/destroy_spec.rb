c42565509a6ae7e46c36a02922243be47ec96fb7
Leonis0813 <Leonis.0813@gmail.com>
2017-04-23 05:21:24 +0100

    modify resource name for delete payment

--- spec/controllers/payment/destroy_spec.rb
+++ spec/controllers/payment/destroy_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 describe PaymentsController, :type => :controller do
-  shared_context '家計簿を削除する' do |id, app_auth_header = CommonHelper.app_auth_header|
+  shared_context '収支情報を削除する' do |id, app_auth_header = CommonHelper.app_auth_header|
     before(:all) do
       client.header('Authorization', app_auth_header)
       @res = client.delete("/payments/#{id}")
@@ -14,18 +14,18 @@ describe PaymentsController, :type => :controller do
   include_context '事前準備: 家計簿を登録する'
 
   describe '正常系' do
-    include_context '家計簿を削除する', PaymentHelper.test_payment[:income][:id]
+    include_context '収支情報を削除する', PaymentHelper.test_payment[:income][:id]
     it_behaves_like 'ステータスコードが正しいこと', '204'
   end
 
   describe '異常系' do
     context 'Authorizationヘッダーがない場合' do
-      include_context '家計簿を削除する', PaymentHelper.test_payment[:income][:id], nil
+      include_context '収支情報を削除する', PaymentHelper.test_payment[:income][:id], nil
       it_behaves_like '400エラーをチェックする', ['absent_header']
     end
 
     context '存在しないidを指定した場合' do
-      include_context '家計簿を削除する', 100
+      include_context '収支情報を削除する', 100
       it_behaves_like 'ステータスコードが正しいこと', '404'
     end
   end
