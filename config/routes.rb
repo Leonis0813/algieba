62aeedf5650537242208e7e4b7fee4a047810328
Leonis0813 <Leonis.0813@gmail.com>
2017-10-08 04:33:52 +0100

    add scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories, :only => [:index]
-  resources :payments, :except => [:new, :edit], :defaults => {:format => 'json'}
-  get '/settlement' => 'payments#settle', :defaults => {:format => 'json'}
-  get '/login' => 'login#form'
-  post '/login' => 'login#authenticate_user'
+  scope '/algieba' do
+    resources :categories, :only => [:index]
+    resources :payments, :except => [:new, :edit], :defaults => {:format => 'json'}
+    get '/settlement' => 'payments#settle', :defaults => {:format => 'json'}
+    get '/login' => 'login#form'
+    post '/login' => 'login#authenticate_user'
+  end
 end
