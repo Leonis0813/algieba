d8bc9f0f6c2568abff1086e70e70c5e4286ce5a2
Leonis0813 <Leonis.0813@gmail.com>
2017-10-08 11:51:24 +0100

    add scoep for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories, :only => [:index]
-  resources :payments, :except => [:new, :edit], :defaults => {:format => 'json'}
-  get '/settlement' => 'payments#settle', :defaults => {:format => 'json'}
+  scope :api, :format => 'json' do
+    resources :categories, :only => [:index]
+    resources :payments, :except => [:new, :edit]
+    get '/settlement' => 'payments#settle'
+  end
+
+  resources :payments, :only => [:index], :format => 'html'
   get '/login' => 'login#form'
   post '/login' => 'login#authenticate_user'
 end
