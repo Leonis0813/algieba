4c4a2f1bd80b3d066e9493dc868fb51d51768dff
Leonis0813 <Leonis.0813@gmail.com>
2017-10-08 11:54:37 +0100

    scope to namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  scope :api, :format => 'json' do
+  namespace :api, :format => 'json' do
     resources :categories, :only => [:index]
     resources :payments, :except => [:new, :edit]
     get '/settlement' => 'payments#settle'
   end
 
   resources :payments, :only => [:index], :format => 'html'
-  get '/login' => 'login#form'
+  get '/login' => 'login#form', :format => 'html'
   post '/login' => 'login#authenticate_user'
 end
