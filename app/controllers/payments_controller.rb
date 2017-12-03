855576a4aa9490700e3ee4655bcf8e175e1f6f5f
Leonis0813 <Leonis.0813@gmail.com>
2017-12-03 14:10:29 +0000

    add per_page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,13 +3,14 @@ class PaymentsController < ApplicationController
 
   def index
     @search_form = Query.new(params.permit(*index_params))
+    @per_page = request.query_parameters[:per_page] || 50
     if @search_form.valid?
       @payments = index_params.inject(Payment.all) do |payments, key|
         value = @search_form.send(key)
         value ? payments.send(key, value) : payments
       end
       @payment = Payment.new
-      @payments = @payments.order(:date => :desc).page(params[:page])
+      @payments = @payments.order(:date => :desc).page(params[:page]).per(@per_page)
       render :status => :ok
     else
       raise BadRequest.new(@search_form.errors.messages.keys.map {|key| "invalid_param_#{key}" })
