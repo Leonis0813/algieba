bcb85087bdcdca68802407ff596ab018b7d4d117
Leonis0813 <Leonis.0813@gmail.com>
2018-01-01 06:34:04 +0000

    update view test case

---
+++ spec/views/statistics/show.html.erb_spec.rb
@@ -0,0 +1,33 @@
+# coding: utf-8
+require 'rails_helper'
+
+describe 'statistics/show', :type => :view do
+  html = nil
+
+  before(:each) do
+    render
+    html ||= response
+  end
+
+  describe '<html><body>' do
+    before(:all) { html = nil }
+
+    describe '<div>' do
+      it '<a>タグがあること' do
+        expect(html).to have_selector('//div/a[href="/algieba/payments"]')
+      end
+
+      describe '<a>' do
+        button_xpath = '//div/a/button[@class="btn btn-default btn-sm"]'
+
+        it '<button>タグがあること' do
+          expect(html).to have_selector(button_xpath)
+        end
+
+        it '<span>タグがあること' do
+          expect(html).to have_selector("#{button_xpath}/span[@class='glyphicon glyphicon-edit']")
+        end
+      end
+    end
+  end
+end
