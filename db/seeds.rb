55f300b45278d20d6c8f2d64bb28abfea2d1b1f2
Leonis0813 <Leonis.0813@gmail.com>
2018-05-30 13:18:46 +0100

    remove seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,6 @@ when 'development'
     Category.new(:name => 'zosma'),
     Payment.new(:id => 1, :payment_type => 'expense', :date => '1000-01-01', :content => 'システムテスト用データ', :price => 100),
     Payment.new(:id => 2, :payment_type => 'income', :date => '1000-01-01', :content => 'システムテスト用データ', :price => 100),
-    User.new(:id => 1, :user_id => 'test_user_id', :password => 'test_user_pass'),
-    Client.new(:id => 1, :application_id => 'test_app_id', :application_key => 'test_app_key'),
-  ].each do |object|
-    begin
-      object.save!
-    rescue ActiveRecord::RecordNotUnique => e
-      puts "[Warning] #{e.message}"
-    end
-  end
-when 'production'
-  [
-    User.new(:id => 1, :user_id => 'leonis', :password => '0qpalzm1'),
-  ].each do |object|
-    begin
-      object.save!
-    rescue ActiveRecord::RecordNotUnique => e
-      puts "[Warning] #{e.message}"
-    end
-  end
-
-  [
-   Client.new(:id => 1, :application_id => '68c58a4f26cb84bd', :application_key => 'a469856b9b1b873a5230a0e1b36ee170'),
-   Client.new(:id => 2, :application_id => '506a160d625ce4ce', :application_key => '63676749f84adaf76f698f35ed3dacaf'),
   ].each do |object|
     begin
       object.save!
