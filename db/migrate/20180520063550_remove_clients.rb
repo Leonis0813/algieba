9d2332872924df575c53e584ea0a3e996d8dccc0
Leonis0813 <Leonis.0813@gmail.com>
2019-05-01 17:06:54 +0000

    fix Rails/ReversibleMigration

--- db/migrate/20180520063550_remove_clients.rb
+++ db/migrate/20180520063550_remove_clients.rb
@@ -1,5 +1,14 @@
 class RemoveClients < ActiveRecord::Migration
-  def change
+  def up
     drop_table :clients
+  end
+
+  def down
+    create_table :clients do |t|
+      t.string :application_id
+      t.string :application_key
+
+      t.timestamps
+    end
   end
 end
