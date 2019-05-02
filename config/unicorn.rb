18e1a40aee813ed54facebd370106f362b76bb98
Leonis0813 <Leonis.0813@gmail.com>
2019-05-02 02:54:02 +0000

    fix deploy error

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ preload_app true
 listen File.expand_path('tmp/sockets/unicorn.sock', ENV['RAILS_ROOT'])
 pid File.expand_path('tmp/pids/unicorn.pid', ENV['RAILS_ROOT'])
 
-before_fork do
+before_fork do |_, _|
   Signal.trap 'TERM' do
     Rails.logger.info('Unicorn master intercepting TERM and sending myself QUIT instead')
     Process.kill 'QUIT', Process.pid
@@ -15,7 +15,7 @@ before_fork do
     ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do
+after_fork do |_, _|
   Signal.trap 'TERM' do
     Rails.logger.info('Unicorn worker intercepting TERM and doing nothing. ' \
                       'Wait for master to send QUIT')
