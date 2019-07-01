ead7617b47be2ec17584b204ee01216aaa7dbe47
Leonis0813 <Leonis.0813@gmail.com>
2019-07-01 11:05:08 +0000

    create models

---
+++ app/models/dictionary.rb
@@ -0,0 +1,9 @@
+class Dictionary < ActiveRecord::Base
+  has_many :category_dictionaries, dependent: :destroy
+  has_many :categories, through: :category_dictionaries
+
+  validates :phrase, :condition,
+            presence: {message: 'absent'}
+  validates :condition,
+            inclusion: {in: %w[equal include], message: 'invalid'}
+end
