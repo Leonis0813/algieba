65c14cfd3b6bb72d5c777b090561147767934cb9
Leonis0813 <Leonis.0813@gmail.com>
2019-07-04 12:15:45 +0000

    add dictionary model spec

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :category_payments, dependent: :destroy
   has_many :payments, through: :category_payments
+  has_many :category_dictionaries, dependent: :destroy
+  has_many :dictionaries, through: :category_dictionaries
 end
--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -2,8 +2,9 @@ class Dictionary < ActiveRecord::Base
   has_many :category_dictionaries, dependent: :destroy
   has_many :categories, through: :category_dictionaries
 
-  validates :phrase, :condition,
-            presence: {message: 'absent'}
+  validates :phrase,
+            presence: {message: 'invalid'}
+
   validates :condition,
             inclusion: {in: %w[equal include], message: 'invalid'}
 end
