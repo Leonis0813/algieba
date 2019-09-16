50a348b1c621ef62a7ff880bfaa848297b99e227
Leonis0813 <Leonis.0813@gmail.com>
2019-09-16 04:14:27 +0000

    create controller spec for settlements

---
+++ spec/controllers/api/settlement/period_spec.rb
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+require 'rails_helper'
+
+describe Api::SettlementsController, type: :controller do
+  shared_context '収支を計算する' do |params = {}|
+    before(:all) do
+      res = client.get('/api/settlements/period', params)
+      @response_status = res.status
+      @response_body = JSON.parse(res.body) rescue res.body
+    end
+  end
+
+  include_context '収支情報を登録する'
+
+  describe '正常系' do
+    [
+      ['yearly', {'settlements' => [{'date' => '1000', 'price' => 900}]}],
+      ['monthly', {'settlements' => [{'date' => '1000-01', 'price' => 900}]}],
+      [
+        'daily',
+        {
+          'settlements' => [
+            {'date' => '1000-01-01', 'price' => 1000},
+            {'date' => '1000-01-02', 'price' => 0},
+            {'date' => '1000-01-03', 'price' => 0},
+            {'date' => '1000-01-04', 'price' => 0},
+            {'date' => '1000-01-05', 'price' => -100},
+          ],
+        },
+      ],
+    ].each do |interval, expected_body|
+      context "#{interval}を指定する場合" do
+        include_context '収支を計算する', interval: interval
+        it_behaves_like 'レスポンスが正しいこと', body: expected_body
+      end
+    end
+  end
+
+  describe '異常系' do
+    [[nil, 'absent'], %w[invalid_interval invalid]].each do |interval, message|
+      context "#{interval || 'nil'}を指定する場合" do
+        body = {'errors' => [{'error_code' => "#{message}_param_interval"}]}
+        include_context '収支を計算する', interval: interval
+        it_behaves_like 'レスポンスが正しいこと', status: 400, body: body
+      end
+    end
+
+    context 'interval パラメーターがない場合' do
+      body = {'errors' => [{'error_code' => 'absent_param_interval'}]}
+      include_context '収支を計算する'
+      it_behaves_like 'レスポンスが正しいこと', status: 400, body: body
+    end
+  end
+end
--- spec/controllers/statistics/show_spec.rb
+++ spec/controllers/statistics/show_spec.rb
@@ -3,15 +3,12 @@
 require 'rails_helper'
 
 describe StatisticsController, type: :controller do
-  shared_context '収支を取得する' do
+  describe '正常系' do
     before(:all) do
       res = client.get('/statistics')
       @response_status = res.status
     end
-  end
 
-  describe '正常系' do
-    include_context '収支を取得する'
     it_behaves_like 'ステータスコードが正しいこと', 200
   end
 end
