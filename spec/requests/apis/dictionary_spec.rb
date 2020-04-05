e729d39725b91fa7d9949481a3a0d3782aa44ee3
Leonis0813 <Leonis.0813@gmail.com>
2020-04-05 13:51:57 +0000

    create api request spec

---
+++ spec/requests/apis/dictionary_spec.rb
@@ -0,0 +1,108 @@
+# coding: utf-8
+
+require 'rails_helper'
+
+describe '辞書情報APIのテスト', type: :request do
+  content = "システムテスト用データ_#{Time.now.to_i}"
+  payment = {
+    payment_type: 'expense',
+    date: '1000-01-01',
+    content: content,
+    categories: ['algieba'],
+    price: 100,
+  }
+
+  shared_context '辞書情報を作成する' do |body|
+    before(:all) do
+      header = app_auth_header.merge(content_type_json)
+      res = http_client.post("#{base_url}/api/dictionaries", body.to_json, header)
+      @response_status = res.status
+      @response_body = JSON.parse(res.body) rescue res.body
+    end
+  end
+
+  shared_context '辞書情報を検索する' do |query|
+    before(:all) do
+      res = http_client.get("#{base_url}/api/dictionaries", query, app_auth_header)
+      @response_status = res.status
+      @response_body = JSON.parse(res.body) rescue res.body
+    end
+  end
+
+  shared_examples '辞書情報作成時のレスポンスが正しいこと' do |expected_body|
+    it_behaves_like 'ステータスコードが正しいこと', 201
+
+    it_is_asserted_by do
+      @response_body.keys.sort == DictionaryHelper.response_keys
+    end
+
+    expected_body.each do |key, value|
+      it "#{key}が#{value}であること" do
+        is_asserted_by { @response_body[key.to_s] == value }
+      end
+    end
+  end
+
+  shared_examples '辞書情報検索時のレスポンスが正しいこと' do |expected_phrases = []|
+    it_behaves_like 'ステータスコードが正しいこと', 200
+
+    it_is_asserted_by { @response_body.keys.sort == %w[dictionaries] }
+
+    it do
+      @response_body['dictionaries'].each do |dictionary|
+        is_asserted_by { dictionary.keys.sort == DictionaryHelper.response_keys }
+      end
+    end
+
+    expected_phrases.each do |phrase|
+      it "phraseが#{phrase}の辞書が含まれていること" do
+        is_asserted_by do
+          @response_body['dictionaries'].any? do |dictionary|
+            dictionary['phrase'] == phrase
+          end
+        end
+      end
+    end
+  end
+
+  include_context '収支情報を作成する', payment
+
+  after(:all) { delete_payments }
+
+  describe '辞書情報を作成する' do
+    body = {phrase: content, condition: 'equal', categories: ['algieba']}
+    include_context '辞書情報を作成する', body
+    it_behaves_like '辞書情報作成時のレスポンスが正しいこと', body.except(:categories)
+  end
+
+  describe '辞書情報を作成する' do
+    body = {phrase: content[1 .. -1], condition: 'include', categories: ['algieba']}
+    include_context '辞書情報を作成する', body
+    it_behaves_like '辞書情報作成時のレスポンスが正しいこと', body.except(:categories)
+  end
+
+  describe '辞書情報を検索する' do
+    include_context '辞書情報を検索する'
+    it_behaves_like '辞書情報検索時のレスポンスが正しいこと'
+  end
+
+  describe 'contentを指定して辞書情報を検索する' do
+    include_context '辞書情報を検索する', {content: content}
+    it_behaves_like '辞書情報検索時のレスポンスが正しいこと', [content, content[1 .. -1]]
+  end
+
+  describe 'phraseを指定して辞書情報を検索する' do
+    include_context '辞書情報を検索する', {phrase: content}
+    it_behaves_like '辞書情報検索時のレスポンスが正しいこと', [content]
+  end
+
+  describe 'conditionを指定して辞書情報を検索する' do
+    include_context '辞書情報を検索する', {condition: 'include'}
+    it_behaves_like '辞書情報検索時のレスポンスが正しいこと', [content[1 .. -1]]
+  end
+
+  describe 'phrase,conditionを指定して辞書情報を検索する' do
+    include_context '辞書情報を検索する', {phrase: content, condition: 'equal'}
+    it_behaves_like '辞書情報検索時のレスポンスが正しいこと', [content]
+  end
+end
---
+++ spec/requests/apis/payment_spec.rb
@@ -0,0 +1,109 @@
+# coding: utf-8
+
+require 'rails_helper'
+
+describe '収支情報APIのテスト', type: :request do
+  category_name = 'algieba'
+  valid_payment = {
+    payment_type: 'expense',
+    date: '1000-01-01',
+    content: 'システムテスト用データ',
+    categories: [category_name],
+    price: 100,
+  }
+  invalid_payment = {
+    payment_type: 'expense',
+    date: 'invalid_date',
+    categories: [category_name],
+    price: 100,
+  }
+
+  shared_context '収支情報を取得する' do
+    before(:all) do
+      url = "#{base_url}/api/payments/#{@payment_id}"
+      res = http_client.get(url, nil, app_auth_header)
+      @response_status = res.status
+      @response_body = JSON.parse(res.body) rescue res.body
+    end
+  end
+
+  shared_examples '収支情報のレスポンスが正しいこと' do |status: 200|
+    it_behaves_like 'ステータスコードが正しいこと', status
+
+    it_is_asserted_by { @response_body.keys.sort == PaymentHelper.response_keys }
+
+    it do
+      @response_body['categories'].each do |category|
+        is_asserted_by { category.keys.sort == CategoryHelper.response_keys }
+      end
+    end
+
+    it do
+      @response_body['tags'].each do |tag|
+        is_asserted_by { tag.keys.sort == TagHelper.response_keys }
+      end
+    end
+  end
+
+  describe '不正な収支情報を作成する' do
+    errors = [{'error_code' => 'absent_param_content'}]
+    include_context '収支情報を作成する', invalid_payment
+    it_behaves_like 'レスポンスが正しいこと', status: 400, body: {'errors' => errors}
+  end
+
+  describe '収支情報を登録する' do
+    include_context '収支情報を作成する', valid_payment
+    before(:all) { @payment_id = @response_body['payment_id'] }
+    it_behaves_like '収支情報のレスポンスが正しいこと', status: 201
+
+    describe '収支情報を取得する' do
+      include_context '収支情報を取得する'
+      it_behaves_like '収支情報のレスポンスが正しいこと'
+    end
+
+    describe '収支情報を更新する' do
+      before(:all) do
+        url = "#{base_url}/api/payments/#{@payment_id}"
+        body = {categories: ['other']}.to_json
+        header = app_auth_header.merge(content_type_json)
+        res = http_client.put(url, body, header)
+        @response_status = res.status
+        @response_body = JSON.parse(res.body) rescue res.body
+      end
+
+      it_behaves_like '収支情報のレスポンスが正しいこと'
+
+      it '収支情報が更新されていること' do
+        is_asserted_by { @response_body['categories'].first['name'] == 'other' }
+      end
+    end
+
+    describe '収支情報を検索する' do
+      query = {
+        payment_type: 'income',
+        page: 1,
+        per_page: 100,
+        sort: 'price',
+        order: 'desc',
+      }
+      include_context 'GET /api/payments', query
+      it_behaves_like '収支検索時のレスポンスが正しいこと'
+    end
+
+    describe '収支情報を削除する' do
+      before(:all) do
+        url = "#{base_url}/api/payments/#{@payment_id}"
+        res = http_client.delete(url, nil, app_auth_header)
+        @response_status = res.status
+        @response_body = JSON.parse(res.body) rescue res.body
+      end
+
+      it_behaves_like 'レスポンスが正しいこと', status: 204, body: ''
+
+      describe '収支情報を取得する' do
+        include_context '収支情報を取得する'
+        it_behaves_like 'レスポンスが正しいこと', status: 404, body: ''
+      end
+    end
+  end
+end
