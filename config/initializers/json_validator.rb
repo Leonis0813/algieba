551b09c53becfaf5f0b6fcde0d9693c3d9c1b74b
Leonis0813 <Leonis.0813@gmail.com>
2020-07-12 15:01:28 +0000

    fix bug

--- config/initializers/json_validator.rb
+++ config/initializers/json_validator.rb
@@ -2,7 +2,7 @@ date = lambda {|value|
   begin
     Date.parse(value)
   rescue ArgumentError
-    raise JSON::Schema::CustomFormatError 'must be in format: YYYY-MM-DD'
+    raise JSON::Schema::CustomFormatError, 'must be in format: YYYY-MM-DD'
   end
 }
 JSON::Validator.register_format_validator(:date, date)
